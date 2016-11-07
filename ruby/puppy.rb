490e8379f0450d36de192ba3fdda9d517fd7e3b1
Jonah Price <jyoonah@gmail.com>
2016-11-06 21:06:51 -0800

    Created puppy clas outline in comments.

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# SPECIES ------------------------
+# Canine Dogous
+#
+# CHARACTERISTICS ----------------
+# Fur Color: varies
+# Fur Length: varies
+# Friendly: yes
+# Eye count: 2
+# Legs: 4
+# Name: varies
+#
+# BEHAVIOR -----------------------
+# Jump
+# Hug
+# Scamper
+# Bark
+# Growl
