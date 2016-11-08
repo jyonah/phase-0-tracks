717f3955f4a33db1d0b716dee88b1173182f3ba6
Jonah Price <jyoonah@gmail.com>
2016-11-07 19:33:10 -0800

    Created all sorts of getter and setter methods with a few other methods that also modify attributes.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -17,16 +17,61 @@ class Santa
     puts "That was a good #{cookie}!"
   end #eat_milk_and_cookies
 
+  def celebrate_birthday
+    puts "I Looooooove cake!"
+    @age += 1
+  end
+
+  def get_mad_at(reindeer)
+    puts "#{reindeer}!!!!"
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking << reindeer
+  end
+
+  #getter methods
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+  
+  def reindeer_ranking
+    @reindeer_ranking
+  end
+
+  def gender
+    @gender
+  end
+
+  #setter methods
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
 end #santa
 
+nicholas = Santa.new("Nicholas", "male", "caucasian")
 
-santas = []
-santa_names = ['Nicholas', 'Jenny', 'Wanda', 'Wilbur']
-santa_genders = ["transman", "gender fluid", "male", "bigender"]
-santa_ethnicities = ["black", "Latino", "white", "Japanese-African"]
+p nicholas.age
+nicholas.celebrate_birthday
+p nicholas.age
 
-santa_names.length.times do |i|
-  santas << Santa.new(santa_names[i], santa_genders[i], santa_ethnicities[i])
-end
+p nicholas.reindeer_ranking
+nicholas.get_mad_at("Vixen")
+p nicholas.reindeer_ranking
 
-p santas
+p nicholas.gender
+nicholas.gender=("tranwoman")
+p nicholas.gender
+# santas = []
+# santa_names = ['Nicholas', 'Jenny', 'Wanda', 'Wilbur']
+# santa_genders = ["transman", "gender fluid", "male", "bigender"]
+# santa_ethnicities = ["black", "Latino", "white", "Japanese-African"]
+#
+# santa_names.length.times do |i|
+#   santas << Santa.new(santa_names[i], santa_genders[i], santa_ethnicities[i])
+# end
+#
+# p santas
