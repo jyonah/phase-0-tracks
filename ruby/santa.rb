1ec223a8219de6980f7f313fc8c23ae6f6b856bf
Jonah Price <jyoonah@gmail.com>
2016-11-07 18:48:56 -0800

    added attributes, and a loop to initialize 4 diverse santas.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,13 @@
 class Santa
+  def initialize(name, gender, ethnicity)
+    puts "Initializing Santa instance ..."
+    @name = name
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end #initialize
+
 
   def speak
     puts "Ho, ho, ho! Haaappy Holidays!"
@@ -8,12 +17,16 @@ class Santa
     puts "That was a good #{cookie}!"
   end #eat_milk_and_cookies
 
-  def initialize
-    puts "Initializing Santa instance ..."
-  end
+end #santa
+
+
+santas = []
+santa_names = ['Nicholas', 'Jenny', 'Wanda', 'Wilbur']
+santa_genders = ["transman", "gender fluid", "male", "bigender"]
+santa_ethnicities = ["black", "Latino", "white", "Japanese-African"]
+
+santa_names.length.times do |i|
+  santas << Santa.new(santa_names[i], santa_genders[i], santa_ethnicities[i])
 end
 
-nicholas = Santa.new
-
-nicholas.speak
-nicholas.eat_milk_and_cookies('snickerdoodle')
+p santas
