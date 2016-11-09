986c043e927c1b96f212a2a137b60f4edc09331e
Jonah Price <jyoonah@gmail.com>
2016-11-08 18:23:31 -0800

    Made Shout a mixin and included in two classes, testing with driver code.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,40 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     puts words + "!!!" + " :("
+#   end
+#
+#   def self.yelling_happily(words)
+#      puts words + "!!!" + " :)"
+#   end
+# end
+#
+# Shout.yell_angrily("I'm mad at you")
+#
+# Shout.yell_angrily("I'm on top of the world")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    puts words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
-    words + "!!!" + " :)"
+  def yelling_happily(words)
+    puts words + "!!!" + " :)"
   end
 end
 
-Shout.yell_angrily("I'm mad at you")
+class Jonah
+  include Shout
+end
 
-Shout.yell_angrily("I'm on top of the world")
+class Steven
+  include Shout
+end
+
+
+person_1 = Jonah.new
+person_1.yell_angrily('Oh my god')
+person_1.yelling_happily('Hillary won')
+
+person_2 = Steven.new
+person_2.yell_angrily('Trump won')
+person_2.yelling_happily('Cake')
