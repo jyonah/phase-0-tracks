93f98cfaa43a374edc7e26cfbe4b665c0d95ec2d
Jonah Price <jyoonah@gmail.com>
2016-11-14 11:32:31 -0800

    Created an array of horse colors and horse names, then pushed an additional value to each.

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var horseColors = ["brown", "white", "grey", "black"]
+var horseNames = ["Jeff", "Steven", "Ohad", "Yoav"]
+
+horseColors.push("red")
+horseNames.push("Jonah")
+
+
+console.log(horseNames)
+console.log(horseColors)
