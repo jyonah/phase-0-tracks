a5a8d5c2ee486d0d75711e1fc80628cc0cb0ea4d
Jonah Price <jyoonah@gmail.com>
2016-12-03 19:15:26 -0800

    WE DID ALL THE THINGS.

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,11 +17,25 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/students/delete' do
+  erb :delete_student
+end
+
 # create new students via
 # a form
-post '/students' do
+post '/students_new' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
 
+post '/students_delete' do
+  db.execute("DELETE FROM students WHERE name=? AND campus=?", params['name'], params['campus'])
+  redirect '/new_home'
+end
+
 # add static resources
+
+get '/new_home' do
+  @students = db.execute("SELECT * FROM students")
+  erb :new_home
+end
